644cf024e2f8ac701c52b798e82f9f7e0a142e12
rjulian <richard@rjulian.net>
2020-09-13 14:47:13 -0700

    Semi-automated multi account push

--- source/reflex_aws_cloudtrail_log_file_validation_disabled.py
+++ source/reflex_aws_cloudtrail_log_file_validation_disabled.py
@@ -4,7 +4,7 @@ import json
 import os
 
 import boto3
-from reflex_core import AWSRule
+from reflex_core import AWSRule, subscription_confirmation
 
 
 class CloudTrailLogFileValidationDisabled(AWSRule):
@@ -33,5 +33,9 @@ class CloudTrailLogFileValidationDisabled(AWSRule):
 
 def lambda_handler(event, _):
     """ Handles the incoming event """
+    print(event)
+    if subscription_confirmation.is_subscription_confirmation(event):
+        subscription_confirmation.confirm_subscription(event)
+        return
     rule = CloudTrailLogFileValidationDisabled(json.loads(event["Records"][0]["body"]))
     rule.run_compliance_rule()
